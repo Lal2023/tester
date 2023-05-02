fbf590a3d4c0d169abed2315f53d65fc69d30e91
Lal2023 <117145151+Lal2023@users.noreply.github.com>
2023-05-02 19:50:45 +1000

    Add Loop
    Added a loop to iterate for 10 cycles

--- program.cpp
+++ program.cpp
@@ -5,4 +5,7 @@ using namespace std;
 int main(){
     cout << "This is the configuration file" << endl;
     cout << "Version 1.0" << endl;
+    for (int i=0;i<10;i++){
+        cout << "The index is " << i << endl;
+    }
 }
